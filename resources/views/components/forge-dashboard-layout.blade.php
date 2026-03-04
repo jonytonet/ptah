70c3491f67598e3fbfd9a86bdd90be32ecbb14f1
Jony Tonet <jonytonet@gmail.com>
2026-03-04 12:12:46 -0300

    fix: escape @teleport in JS comment inside x-data (Blade parsing error)

--- resources/views/components/forge-dashboard-layout.blade.php
+++ resources/views/components/forge-dashboard-layout.blade.php
@@ -421,7 +421,7 @@
             })(),
 
             init() {
-                /* Aplica ptah-dark no body para cobrir elementos @teleport('body') */
+                /* Aplica ptah-dark no body para cobrir elementos @@teleport('body') */
                 document.body.classList.toggle('ptah-dark', this.darkMode);
 
                 /* Reagir a mudanças de preferência do SO em tempo real */
